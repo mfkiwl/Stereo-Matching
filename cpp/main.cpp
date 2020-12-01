1623b90b3c0fecfe96974958f8e51a50b5058201
jerryweihuajing <jerryweihuajing@126.com>
2020-12-01 18:12:45 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -19,7 +19,27 @@ Created on Mon Nov 11 14:51:37 2020
 
 #include "Header\calculation_feature_matching.h"
 
+Mat CenterROI(Mat& image_gray) {
 
+	Point_<int> center_ROI;
+	center_ROI.x = int(image_gray.cols / 2);
+	center_ROI.y = int(image_gray.rows / 2);
+
+	//ROI factor
+	double zoom_factor = 0.9;
+
+	//define ROI size
+	int width_ROI = int(image_gray.cols * zoom_factor);
+	int height_ROI = int(image_gray.rows * zoom_factor);
+
+	//define ROI parameter
+	Rect rect(center_ROI.x - int(width_ROI / 2),
+		center_ROI.y - int(height_ROI / 2),
+		width_ROI,
+		height_ROI);
+
+	return image_gray(rect);
+}
 Mat DisparityMapBM(Mat& image_left, Mat& image_right) {
 
 	//gray level to calculate disparity
@@ -32,25 +52,10 @@ Mat DisparityMapBM(Mat& image_left, Mat& image_right) {
 	image_gray_left = image_gray_left;
 	image_gray_right = image_gray_right;
 
-	Point_<int> center_ROI;
-	center_ROI.x = int(image_left.cols / 2);
-	center_ROI.y = int(image_left.rows / 2);
-	
-	//ROI factor
-	double zoom_factor = 0.31;
-
-	//define ROI size
-	int width_ROI = int(image_left.cols * zoom_factor);
-	int height_ROI = int(image_left.rows * zoom_factor);
-
-	//define ROI parameter
-	Rect rect(center_ROI.x - int(width_ROI / 2),
-		center_ROI.y - int(height_ROI / 2),
-		width_ROI,
-		height_ROI);
-
+	//Mat of disparity
 	Mat disparity, disparity_8U;
 
+	int block_size = 9;
 	int num_disparities = 16 * 8;
 	int uniqueness_ratio = 15;
 
@@ -59,7 +64,7 @@ Mat DisparityMapBM(Mat& image_left, Mat& image_right) {
 	//BM->setROI1(ROI);
 	//BM->setROI2(ROI);
 	BM->setPreFilterCap(31);
-	BM->setBlockSize(9);
+	BM->setBlockSize(block_size);
 	BM->setMinDisparity(-16);
 	BM->setNumDisparities(num_disparities);
 	BM->setTextureThreshold(10);
@@ -67,7 +72,7 @@ Mat DisparityMapBM(Mat& image_left, Mat& image_right) {
 	BM->setSpeckleWindowSize(100);
 	BM->setSpeckleRange(32);
 	BM->setDisp12MaxDiff(1);
-	BM->compute(image_gray_left(rect), image_gray_right(rect), disparity);
+	BM->compute(CenterROI(image_gray_left), CenterROI(image_gray_right), disparity);
 
 	disparity.convertTo(disparity_8U, CV_8UC1, 255 / (num_disparities));
     
@@ -77,7 +82,51 @@ Mat DisparityMapBM(Mat& image_left, Mat& image_right) {
 
 	return disparity;
 }
+Mat DisparityMapSGBM(Mat& image_left, Mat& image_right) {
 
+	//gray level to calculate disparity
+	Mat image_gray_left;
+	Mat image_gray_right;
+
+	cvtColor(image_left, image_gray_left, COLOR_BGR2GRAY);
+	cvtColor(image_right, image_gray_right, COLOR_BGR2GRAY);
+
+	image_gray_left = image_gray_left;
+	image_gray_right = image_gray_right;
+
+	//Mat of disparity
+	Mat disparity, disparity_8U;
+	int block_size = 9;
+	int num_disparities = 16 * 8;
+	int uniqueness_ratio = 15;
+	int P1 = image_left.channels() * block_size * block_size * 8;
+	int P2 = image_left.channels() * block_size * block_size * 32;
+
+	Ptr<StereoSGBM> SGBM = StereoSGBM::create(16, 9);
+
+	//BM->setROI1(ROI);
+	//BM->setROI2(ROI);
+	SGBM->setPreFilterCap(31);
+	SGBM->setBlockSize(block_size);
+	SGBM->setMinDisparity(-16);
+	SGBM->setNumDisparities(num_disparities);
+	SGBM->setUniquenessRatio(uniqueness_ratio);
+	SGBM->setSpeckleWindowSize(100);
+	SGBM->setSpeckleRange(32);
+	SGBM->setDisp12MaxDiff(1);
+	SGBM->setP1(P1);
+	SGBM->setP2(P2); 
+	SGBM->setMode(StereoSGBM::MODE_SGBM_3WAY); //MODE_HH MODE_SGBM MODE_SGBM_3WAY
+	SGBM->compute(CenterROI(image_gray_left), CenterROI(image_gray_right), disparity);
+
+	disparity.convertTo(disparity_8U, CV_8UC1, 255 / (num_disparities));
+
+	namedWindow("Disparity (SGBM)", 1);
+	imshow("Disparity (SGBM)", disparity_8U);
+	waitKey(0);
+
+	return disparity;
+}
 int main(){
 	
 	cout << "Built with OpenCV " << CV_VERSION << endl;
@@ -108,11 +157,11 @@ int main(){
 	//Mat image_left = vector_image[0];
 	//Mat image_right = vector_image[1];
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 1; i++) {
 
 		clock_t t1 = clock();
 
-		DisparityMapBM(image_a, image_b);
+		DisparityMapSGBM(image_a, image_b);
 
 		clock_t t2 = clock();
 
