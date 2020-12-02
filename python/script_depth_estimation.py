510bb1cb8f25880044ad4616342600a26c3acb1b
jerryweihuajing <jerryweihuajing@126.com>
2020-12-02 14:14:50 +0800

    Update script_depth_estimation.py

--- python/script_depth_estimation.py
+++ python/script_depth_estimation.py
@@ -23,11 +23,15 @@ folder_path='../Material/'
 image_left=cv2.imread(folder_path+'L14.png',0)
 image_right=cv2.imread(folder_path+'R14.png',0)
 
-for window_size in [1,3,5,7,9,11,13,15,17,19][:2]:
+plt.figure(figsize=(17,31))
+
+count=1
+
+ # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
+for window_size in [1,3,5,7,9,11,13,15,17,19]:
     
     # SGBM Parameters -----------------
-    window_size = 3                     # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-     
+    
     # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
     stereo = cv2.StereoSGBM_create(minDisparity=0,
                                    numDisparities=160,             # max_disp has to be dividable by 16 f. E. HH 192, 256
@@ -41,16 +45,22 @@ for window_size in [1,3,5,7,9,11,13,15,17,19][:2]:
                                    preFilterCap=63,
                                    mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY)
     
-    disparity_left=stereo.compute(image_left,image_right)
-    disparity_right=stereo.compute(image_right,image_left)
+    disparity=stereo.compute(image_left,image_right)
     
-    plt.figure(figsize=(17,6))
+    plt.subplot(5,2,count)
     
-    plt.subplot(121)
-    plt.imshow(disparity_left,'rainbow')
+    plt.imshow(disparity,'rainbow')
+    plt.xticks([])
+    plt.yticks([])
     
-    plt.subplot(122)
-    plt.imshow(disparity_right,'terrain')
+    plt.title('window size: %d'%window_size,fontdict=title_prop)
     
-plt.figure()
+    count+=1
+    
+plt.figure(figsize=(13,6))
 plt.imshow(image_left,'gray')
+
+plt.xticks([])
+plt.yticks([])
+
+plt.title('original image',fontdict=title_prop)
